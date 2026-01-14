0a8b64e2be310cb9de69323445b34e1276934663
zahra-ynp <zahra.ynp@gmail.com>
2026-01-14 18:05:41 +0100

    add java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
