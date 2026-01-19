ad67b2977f24d3eb8e5ea0b65a5c296739c46d19
manasa-santhoshi <manasa.s.maddi@gmail.com>
2026-01-19 17:37:40 +0100

    feature: switch turn after non-scoring move

--- src/main/java/it/units/sdm/dotsandboxes/logic/GameSession.java
+++ src/main/java/it/units/sdm/dotsandboxes/logic/GameSession.java
@@ -1,5 +1,6 @@
 package it.units.sdm.dotsandboxes.logic;
 
+import it.units.sdm.dotsandboxes.model.Move;
 import it.units.sdm.dotsandboxes.model.Player;
 
 public class GameSession {
@@ -9,11 +10,14 @@ public class GameSession {
     public GameSession(int width, int height) {
         // Player always starts
         this.currentPlayer = Player.Player1;
-
     }
 
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
-}
 
+    public void makeMove(Move move) {
+        // Minimal for this iteration: assume no box completed -> switch player
+        currentPlayer = (currentPlayer == Player.Player1) ? Player.Player2 : Player.Player1;
+    }
+}
