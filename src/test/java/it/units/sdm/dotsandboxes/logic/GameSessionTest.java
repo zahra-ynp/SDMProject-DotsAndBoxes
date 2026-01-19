4f7ff5d7671488ebefe75afdcd45cff2916fa1c1
manasa-santhoshi <manasa.s.maddi@gmail.com>
2026-01-19 17:32:23 +0100

    test: switch player after non-scoring move

--- src/test/java/it/units/sdm/dotsandboxes/logic/GameSessionTest.java
+++ src/test/java/it/units/sdm/dotsandboxes/logic/GameSessionTest.java
@@ -1,15 +1,28 @@
 package it.units.sdm.dotsandboxes.logic;
 
+import it.units.sdm.dotsandboxes.model.Direction;
+import it.units.sdm.dotsandboxes.model.Move;
 import it.units.sdm.dotsandboxes.model.Player;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameSessionTest {
 
     @Test
-    void gameStartsWithPlayerOne() {
+    void startsWithPlayerOne() {
         GameSession session = new GameSession(5, 5);
         assertEquals(Player.Player1, session.getCurrentPlayer());
     }
+
+    @Test
+    void switchesPlayerWhenNoBoxIsCompleted() {
+        GameSession session = new GameSession(5, 5);
+
+        // a simple move that cannot complete a box as first move
+        session.makeMove(new Move(0, 0, Direction.HORIZONTAL));
+
+        assertEquals(Player.Player2, session.getCurrentPlayer());
+    }
 }
+
